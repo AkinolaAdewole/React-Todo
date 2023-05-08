59a341e0aaa2377f4a063a87f3c37590733a3f44
Akinola <adewoleakinola24@gmail.com>
2023-05-08 23:43:38 +0100

    cols

--- src/Components/DisplayTodo.js
+++ src/Components/DisplayTodo.js
@@ -38,7 +38,7 @@ const DisplayTodo = ({ allTodo, setallTodo}) => {
   return (
     <>
       <div className="display">
-        <table className="table table-warning table-success table-striped  table-responsive " >
+        <table className="table table-bordered table-success table-responsive table-hover " >
           <thead>
             <tr>
                 <td>S/N</td>
